b783b38431b8d697eca968fc7b2e4905b74a3e93
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2024-03-12 17:16:36 +0500

    Update validator.ts

--- validator.ts
+++ validator.ts
@@ -1,5 +1,5 @@
-import e from "https://deno.land/x/oridune_validator@v0.2.48/mod.ts";
-export * from "https://deno.land/x/oridune_validator@v0.2.48/mod.ts";
+import e from "https://deno.land/x/oridune_validator@v0.2.49/mod.ts";
+export * from "https://deno.land/x/oridune_validator@v0.2.49/mod.ts";
 
 // import e from "file:///D:\\Deno\\Repositories\\validator\\mod.ts";
 // export * from "file:///D:\\Deno\\Repositories\\validator\\mod.ts";
