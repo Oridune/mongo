6afa513abfab7602ef1f62ff8c13837819c15972
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2024-03-19 20:20:52 +0500

    Minor updates

--- validator.ts
+++ validator.ts
@@ -1,5 +1,5 @@
-import e from "jsr:@oridune/validator@^0.2.50";
-export * from "jsr:@oridune/validator@^0.2.50";
+import e from "jsr:@oridune/validator@^0.2.51";
+export * from "jsr:@oridune/validator@^0.2.51";
 
 // import e from "file:///D:\\Deno\\Repositories\\validator\\mod.ts";
 // export * from "file:///D:\\Deno\\Repositories\\validator\\mod.ts";
