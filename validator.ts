6ccbfb078064d2398bf91972890354047cb9f8d6
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2024-03-19 18:58:13 +0500

    published to jsr

--- validator.ts
+++ validator.ts
@@ -1,5 +1,5 @@
-import e from "https://deno.land/x/oridune_validator@v0.2.49/mod.ts";
-export * from "https://deno.land/x/oridune_validator@v0.2.49/mod.ts";
+import e from "jsr:@oridune/validator";
+export * from "jsr:@oridune/validator";
 
 // import e from "file:///D:\\Deno\\Repositories\\validator\\mod.ts";
 // export * from "file:///D:\\Deno\\Repositories\\validator\\mod.ts";
